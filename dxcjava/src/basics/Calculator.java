7215ade62355fc248025d70fc02b127b40bd179b
sarur <sarur@LAPTOP-C1R4F962>
2021-10-19 14:49:42 +0530

    debugdemo

---
+++ dxcjava/src/basics/Calculator.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Calculator {
+	
+		int add(int a, int b) {
+			return a+b;
+		}
+		int sub(int a, int b) {
+			return a-b;
+		}
+		int mul(int a, int b) {
+			return a*b;
+		}
+		int div(int a, int b) {
+			return a/b;
+		}
+
+}
---
+++ dxcjava/src/basics/DebugDemo.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class DebugDemo {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int sum = calc.add(10,20);
+		System.out.println("the sum is --"+sum);
+		throw new NullPointerException();
+		
+		
+	}
+
+}
