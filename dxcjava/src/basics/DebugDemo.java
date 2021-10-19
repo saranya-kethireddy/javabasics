b4febe84549a8fc2dfde7253706f76d7e5bcffb4
sarur <sarur@LAPTOP-C1R4F962>
2021-10-19 15:37:16 +0530

    adding a watch

--- dxcjava/src/basics/DebugDemo.java
+++ dxcjava/src/basics/DebugDemo.java
@@ -6,7 +6,14 @@ public class DebugDemo {
 		Calculator calc = new Calculator();
 		int sum = calc.add(10,20);
 		System.out.println("the sum is --"+sum);
-		throw new NullPointerException();
+		
+		for(int a=0; a<5; a++) {
+			int f = a+ 5 *3;
+			System.out.println("the no is ="+f);
+		}
+		
+		
+		//throw new NullPointerException();
 		
 		
 	}
