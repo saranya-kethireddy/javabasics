a00b83641e35eb5f1b291798a09e3b1f47660536
sarur <sarur@LAPTOP-C1R4F962>
2021-10-19 14:35:53 +0530

    control statements

---
+++ dxcjava/src/controlstatements/IfElseTernaryExample.java
@@ -0,0 +1,9 @@
+package controlstatements;
+
+public class IfElseTernaryExample {
+     public static void main(String[] args) {
+		int number = 13;
+		String output=(number %2 == 0)?"even number":"odd number";
+		System.out.println(output);
+	}
+}
